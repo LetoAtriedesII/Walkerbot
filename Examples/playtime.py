fff336af5cd36dce9ee1ab61656a1cebbb6d7df9
Adam Novak <novakam1>
2019-03-25 14:35:47 -0400

    Updated playtime.py to refect firmware update. Added parseout.txt as an output file for parseWalkingData.py

--- Examples/playtime.py
+++ Examples/playtime.py
@@ -6,6 +6,14 @@
 #
 # Author: Vinman <vinman.wen@ufactory.cc> <vinman.cub@gmail.com>
 
+'''
+stretch (mm)        : 150 -> 300
+rotation (degrees)  :   0 -> 180 
+height (mm)         :  30 -> 150 
+speed (mm/s?)       :   0 -> 250
+'''
+
+
 import os
 import sys
 import time
@@ -16,9 +24,10 @@ swift.waiting_ready(timeout=3)
 device_info = swift.get_device_info()
 print(device_info)
 firmware_version = device_info['firmware_version']
-if firmware_version and not firmware_version.startswith(('0.', '1.', '2.', '3.')):
-    swift.set_speed_factor(0.0005)
+#if firmware_version and not firmware_version.startswith(('0.', '1.', '2.', '3.')):
+#    swift.set_speed_factor(0.0005)
 
+#set for general mode
 swift.set_mode(0)
 
 '''
@@ -28,16 +37,41 @@ swift.set_position(y=100)
 swift.set_position(z=100)
 swift.flush_cmd(wait_stop=True)
 '''
-
+'''
 swift.set_polar(stretch=150, speed=100000)
-swift.set_polar(rotation=90)
+swift.set_polar(rotation=0)
 swift.set_polar(height=80)
-print(swift.set_polar(stretch=150, rotation=90, height=80, wait=True))
+print(swift.set_polar(stretch=150, rotation=0, height=80, wait=True))
 swift.flush_cmd()
+'''
+'''
+swift.set_polar(stretch=150, rotation=90, height=150, speed=100000, wait=True)
+print(swift.set_polar(stretch=150, rotation=90, height=150, wait=True))
 
-swift.set_polar(stretch=150, speed=100000)
-swift.set_polar(rotation=90)
-swift.set_polar(height=120)
-print(swift.set_polar(stretch=150, rotation=90, height=120, wait=True))
+swift.set_polar(stretch=300, rotation=90, height=30, speed=100000, wait=True)
+print(swift.set_polar(stretch=300, rotation=90, height=30, wait=True))
+'''
 
+
+
+swift.set_polar(stretch=150, rotation=0, height=100, speed=250)
+print(swift.set_polar(stretch=150, rotation=0, height=100, wait=True))
+
+swift.set_polar(stretch = 200, rotation=90, height= 50)
+print(swift.set_polar(stretch = 200, rotation=90, height= 50, wait=True))
+
+swift.set_polar(stretch= 300, rotation=180, height=150)
+print(swift.set_polar(stretch= 300, rotation=180, height=150, wait=True))
+
+swift.set_polar(stretch=180, rotation=90, height=100)
+print(swift.set_polar(stretch=180, rotation=90, height=100, wait=True))
+swift.flush_cmd()
+'''
+swift.set_polar(stretch=150, rotation=180, height=100, speed=250, wait=True)
+print(swift.set_polar(stretch=150, rotation=180, height=100, wait=True))
+swift.flush_cmd()
+swift.set_polar(rotation=0, wait=True)
+print(swift.set_polar(rotation=0, wait=True))
+swift.flush_cmd()
+'''
 swift.disconnect()
--- parseWalkingData.py
+++ parseWalkingData.py
@@ -1,5 +1,17 @@
-#this code is designed to parse data from a csv file and
-#create motion commands for the robot
+'''
+Author: Adam Novak
+Description: this code is designed to parse data from a csv file and
+create motion commands for the robot
+Date modified: 03/25/19
+Version: 1.0
+'''
+
+'''
+stretch (mm)        : 150 -> 300
+rotation (degrees)  :   0 -> 180 
+height (mm)         :  30 -> 150 
+speed (mm/s?)       :   0 -> 250
+'''
 
 import csv
 xPos = []
@@ -11,6 +23,7 @@ zDist = []
 xSpeed = []
 ySpeed = []
 zSpeed = []
+file = open('parseout.txt','w') 
 
 with open('bnj_walk.csv') as csvDataFile:
     csvReader = csv.reader(csvDataFile)
@@ -22,12 +35,12 @@ with open('bnj_walk.csv') as csvDataFile:
         ySpeed.append(int(row[5]))
         zSpeed.append(int(row[6]))
 
-for i in range(0, len(xPos)):
+for i in range(0, len(xPos)-1):
     xDist.append(xPos[i] - xPos[i+1])
     yDist.append(yPos[i] - yPos[i+1])
     zDist.append(zPos[i] - zPos[i+1])
-    print('swift.set_polar(stretch=' + str(xDist[i]) + ', rotation=' + str(zDist[i]) + ', height=' + str(zDist[i]) + ', speed=' + str(xSpeed[i]) + ', wait=True)')
+    file.write('swift.set_polar(stretch=' + str(xDist[i]) + ', rotation=' + str(zDist[i]) + ', height=' + str(zDist[i]) + ', speed=' + str(xSpeed[i]) + ', wait=True)\n')
 
 
-
+file.close()
 #end
